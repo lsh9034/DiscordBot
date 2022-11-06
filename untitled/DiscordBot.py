ac56068afe99d86bcab65c18dd91979b573311e6
lsh9034 <lsh9034@kookmin.ac.kr>
2022-11-06 21:21:56 +0900

    Change message commands to slash commands

--- untitled/DiscordBot.py
+++ untitled/DiscordBot.py
@@ -10,6 +10,9 @@ import requests
 import warnings
 from efficientnet_pytorch import EfficientNet
 from matplotlib import cm
+from discord.ext import commands
+from discord import app_commands
+
 
 warnings.filterwarnings('ignore')
 print(torch.__version__)
@@ -23,7 +26,12 @@ discord_token = f.read()
 # discord Client class를 생성합니다.
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
 model = EfficientNet.from_pretrained('efficientnet-b0')
+
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.all(), application_id = 1038780297883435119)
+
 def predict(img):
 
     # Preprocess image
@@ -50,29 +58,28 @@ def predict(img):
         ans = labels_map[idx]
         break
     return ans
+
 # event decorator를 설정하고 on_ready function을 할당해줍니다.
 @client.event
 async def on_ready():  # on_ready event는 discord bot이 discord에 정상적으로 접속했을 때 실행됩니다.
     print('We have logged in as {}'.format(client))
     print('Bot name: {}'.format(client.user.name))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
     print('Bot ID: {}'.format(client.user.id))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
+    await client.wait_until_ready()
+    await tree.sync(guild=discord.Object(id=1038402429735149618))
 
-# event decorator를 설정하고 on_message function을 할당해줍니다.
-@client.event
-async def on_message(message):
-    # message란 discord 채널에 올라오는 모든 message를 의미합니다.
-    # 따라서 bot이 보낸 message도 포함이되죠.
-    # 아래 조건은 message의 author가 bot(=clinet.user)이라면 그냥 return으로 무시하라는 뜻입니다.
-    if message.author == client.user:
-        return
-    # message를 보낸 사람이 bot이 아니라면 message가 hello로 시작하는 경우 채널에 Hello!라는 글자를 보내라는 뜻입니다.
-    elif message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-    elif message.content == 'ImageNet':
-        url = message.attachments[0].url
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content)).convert('RGB')
-        ans = predict(img)
-        await message.channel.send(ans)
+@tree.command(name='hello', description="bot answer Hello + name", guild= discord.Object(id=1038402429735149618))
+async def helloworld(interaction: discord.Interaction, name: str):
+    await interaction.response.send_message(f"Hello {name}! It was made by Discord.py")
+
+@tree.command(name='efficient-net', description="EfficientNet classify class of image", guild= discord.Object(id=1038402429735149618))
+async def helloworld(interaction: discord.Interaction, image: discord.Attachment):
+    url = image.url
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+    ans = predict(img)
+    await interaction.response.send_message(ans)
+
+
 # 위에서 설정한 client class를 token으로 인증하여 실행합니다.
 client.run(discord_token)
